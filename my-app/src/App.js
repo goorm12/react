efeb6f741902e23130712d88dd3038e0bd1f237c
binwoonki <vinwoonki98@gmail.com>
2024-06-21 17:39:03 +0900

    간단하게 toast 만들어보기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,31 @@
-import logo from "./logo.svg";
 import "./App.css";
-import TextComponent from "./components/TextComponent";
-import Section from "./components/Section";
 
 function App() {
   return (
-    <div className="App">
-      <TextComponent />
-      <Section />
+    <div className="wrapper">
+      <div className="toast toast-success">
+        <main className="toast__message">
+          <header className="toast__message-title">Success</header>
+          <p className="toast__message-text">Right On!</p>
+        </main>
+        <button className="toast__button">Dismiss</button>
+      </div>
+      <div className="toast toast-warning">
+        <main className="toast__message">
+          <header className="toast__message-title">Warning</header>
+          <p className="toast__message-text">Hmmm!</p>
+        </main>
+        <button className="toast__button">Dismiss</button>
+      </div>
+      <div className="toast toast-error">
+        <main className="toast__message">
+          <header className="toast__message-title">Error</header>
+          <p className="toast__message-text">Oh!</p>
+        </main>
+        <button className="toast__button">Dismiss</button>
+      </div>
     </div>
   );
 }
+
+export default App;
