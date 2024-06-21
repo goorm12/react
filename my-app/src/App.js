2a9e109248a0518488b1c52d0a48e850cfc8e305
binwoonki <vinwoonki98@gmail.com>
2024-06-21 16:38:01 +0900

    Section.js 생성

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import TextComponent from "./components/TextComponent";
+import Section from "./components/Section";
 
 function App() {
   return (
     <div className="App">
       <TextComponent />
+      <Section />
     </div>
   );
 }
-
-export default App;
