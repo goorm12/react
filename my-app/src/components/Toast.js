5164caef1b9ab5d58ecb0733c366acbd18b6c93f
binwoonki <vinwoonki98@gmail.com>
2024-06-21 19:46:24 +0900

    리팩토링) ToastMessage, ToastButton 두 개로 분리

--- my-app/src/components/Toast.js
+++ my-app/src/components/Toast.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./Toast.css";
+import ToastMessage from "./ToastMessage";
+import ToastButton from "./ToastButton";
 
 const Toast = ({ message }) => {
   return (
     <div className={`toast toast-${message.title}`}>
-      <main className="toast__message">
-        <p className="toast__message-title">{message.title}</p>
-        <p className="toast__message-text">{message.text}</p>
-      </main>
-      <button className="toast__button">Dismiss</button>
+      <ToastMessage message={message} />
+      <ToastButton />
     </div>
   );
 };
