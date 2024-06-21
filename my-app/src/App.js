859b85b3d5dbd7f9ec6eb276aa07ce1a95f828ce
binwoonki <vinwoonki98@gmail.com>
2024-06-21 18:05:12 +0900

    props를 통해서 컴포넌트 사용하기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,38 @@ import "./App.css";
 import Toast from "./components/Toast";
 
 function App() {
+  const messageArray = [
+    {
+      title: "success",
+      text: "Right On!",
+    },
+    {
+      title: "success",
+      text: "Right On!",
+    },
+    {
+      title: "warning",
+      text: "Hmmm",
+    },
+    {
+      title: "warning",
+      text: "Hmmm",
+    },
+    {
+      title: "error",
+      text: "Uh Oh",
+    },
+    {
+      title: "error",
+      text: "Uh Oh",
+    },
+  ];
+
   return (
     <div className="wrapper">
-      <Toast />
-      <Toast />
-      <Toast />
+      {messageArray.map((message) => (
+        <Toast title={message.title} text={message.text} />
+      ))}
     </div>
   );
 }
